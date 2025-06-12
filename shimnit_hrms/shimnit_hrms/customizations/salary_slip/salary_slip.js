06c2900b8b751fa2a61763f8bc033c69eb744d10
ManjitKhatri <manjeetkhatri008@gmail.com>
2025-06-12 17:47:17 +0530

    changes

--- shimnit_hrms/shimnit_hrms/customizations/salary_slip/salary_slip.js
+++ shimnit_hrms/shimnit_hrms/customizations/salary_slip/salary_slip.js
@@ -3,7 +3,6 @@ frappe.ui.form.on('Salary Slip', {
         frm.fields_dict['earnings'].grid.wrapper.find('.grid-row').each(function (i, row_el) {
             const row_data = frm.doc.earnings[i-1];
             if (row_data && row_data.custom_hide_from_salary_slip == 1) {
-                alert(row_data.salary_component)
                 $(row_el).hide(); // hide the HTML element
             }
         });
