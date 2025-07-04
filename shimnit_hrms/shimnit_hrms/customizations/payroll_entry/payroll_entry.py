2e9d698cf8d7ef53c52eb8bd9168a9fc9c947403
BHAVESH MIDOCEAN <bhavesh.m@midocean.tech>
2025-07-04 17:08:21 +0530

    Raghvendra code: JV entry save as draft only using payroll entry

--- shimnit_hrms/shimnit_hrms/customizations/payroll_entry/payroll_entry.py
+++ shimnit_hrms/shimnit_hrms/customizations/payroll_entry/payroll_entry.py
@@ -1,4 +1,134 @@
 import frappe
+import erpnext
+from frappe import _
+from hrms.payroll.doctype.payroll_entry.payroll_entry import PayrollEntry
+from erpnext.accounts.doctype.accounting_dimension.accounting_dimension import (
+	get_accounting_dimensions,
+)
+
+class CustomPayrollEntry(PayrollEntry):
+	def make_accrual_jv_entry(self, submitted_salary_slips):
+		self.check_permission("write")
+		employee_wise_accounting_enabled = frappe.db.get_single_value(
+			"Payroll Settings", "process_payroll_accounting_entry_based_on_employee"
+		)
+		self.employee_based_payroll_payable_entries = {}
+		self._advance_deduction_entries = []
+
+		earnings = (
+			self.get_salary_component_total(
+				component_type="earnings",
+				employee_wise_accounting_enabled=employee_wise_accounting_enabled,
+			)
+			or {}
+		)
+
+		deductions = (
+			self.get_salary_component_total(
+				component_type="deductions",
+				employee_wise_accounting_enabled=employee_wise_accounting_enabled,
+			)
+			or {}
+		)
+
+		precision = frappe.get_precision("Journal Entry Account", "debit_in_account_currency")
+
+		if earnings or deductions:
+			accounts = []
+			currencies = []
+			payable_amount = 0
+			accounting_dimensions = get_accounting_dimensions() or []
+			company_currency = erpnext.get_company_currency(self.company)
+
+			payable_amount = self.get_payable_amount_for_earnings_and_deductions(
+				accounts,
+				earnings,
+				deductions,
+				currencies,
+				company_currency,
+				accounting_dimensions,
+				precision,
+				payable_amount,
+			)
+
+			payable_amount = self.set_accounting_entries_for_advance_deductions(
+				accounts,
+				currencies,
+				company_currency,
+				accounting_dimensions,
+				precision,
+				payable_amount,
+			)
+
+			self.set_payable_amount_against_payroll_payable_account(
+				accounts,
+				currencies,
+				company_currency,
+				accounting_dimensions,
+				precision,
+				payable_amount,
+				self.payroll_payable_account,
+				employee_wise_accounting_enabled,
+			)
+
+			self.make_journal_entry(
+				accounts,
+				currencies,
+				self.payroll_payable_account,
+				voucher_type="Journal Entry",
+				user_remark=_("Accrual Journal Entry for salaries from {0} to {1}").format(
+					self.start_date, self.end_date
+				),
+				submit_journal_entry=False,
+				submitted_salary_slips=submitted_salary_slips,
+			)
+            
+	
+	def make_journal_entry(
+		self,
+		accounts,
+		currencies,
+		payroll_payable_account=None,
+		voucher_type="Journal Entry",
+		user_remark="",
+		submitted_salary_slips: list | None = None,
+		submit_journal_entry=False,
+	) -> str:
+		multi_currency = 0
+		if len(currencies) > 1:
+			multi_currency = 1
+
+		journal_entry = frappe.new_doc("Journal Entry")
+		journal_entry.voucher_type = voucher_type
+		journal_entry.user_remark = user_remark
+		journal_entry.company = self.company
+		journal_entry.posting_date = self.posting_date
+		journal_entry.custom_state = self.state
+
+		journal_entry.set("accounts", accounts)
+		journal_entry.multi_currency = multi_currency
+
+		if voucher_type == "Journal Entry":
+			journal_entry.title = payroll_payable_account
+
+		journal_entry.save(ignore_permissions=True)
+
+		try:
+			if submit_journal_entry:
+				journal_entry.submit()
+
+			if submitted_salary_slips:
+				self.set_journal_entry_in_salary_slips(submitted_salary_slips, jv_name=journal_entry.name)
+
+		except Exception as e:
+			if type(e) in (str, list, tuple):
+				frappe.msgprint(e)
+
+			self.log_error("Journal Entry creation against Salary Slip failed")
+			raise
+
+		return journal_entry
+
 
 @frappe.whitelist()
 def create_bulk_payroll_entry(posting_date, start_date, end_date):
