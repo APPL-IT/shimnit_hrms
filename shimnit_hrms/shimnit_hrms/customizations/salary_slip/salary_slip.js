33529a277b0fdf9787104c2d44938c67740181a1
APPL-IT <raghvendra.aditya@aegisprecision.com>
2025-07-24 11:38:01 +0530

    added hide on  deduction

--- shimnit_hrms/shimnit_hrms/customizations/salary_slip/salary_slip.js
+++ shimnit_hrms/shimnit_hrms/customizations/salary_slip/salary_slip.js
@@ -6,5 +6,11 @@ frappe.ui.form.on('Salary Slip', {
                 $(row_el).hide(); // hide the HTML element
             }
         });
+        frm.fields_dict['deductions'].grid.wrapper.find('.grid-row').each(function (i, row_el) {
+            const row_data = frm.doc.earnings[i-1];
+            if (row_data && row_data.custom_hide_from_salary_slip == 1) {
+                $(row_el).hide(); // hide the HTML element
+            }
+        });
     }
 });
